53a352e05826a7f64d77963aee23686312678977
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-05-27 21:47:17 -0300

    Feat: Create post page

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,3 +1,35 @@
+import { Link } from 'react-router-dom'
+import { Content, InfosPost, PostContainer, TitlePost } from './styles'
+
 export function Post() {
-  return <h1>Post</h1>
+  return (
+    <PostContainer>
+      <TitlePost>
+        <div>
+          <Link to="/">Voltar</Link>
+
+          <a href="#">Ver no Github</a>
+        </div>
+        <h2>JavaScript data types and data structures</h2>
+        <InfosPost>
+          <span>cameronwll</span>
+          <span>Há 1 dia</span>
+          <span>5 comentários</span>
+        </InfosPost>
+      </TitlePost>
+      <Content>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn. Dynamic typing JavaScript is a loosely typed and dynamic
+          language. Variables in JavaScript are not directly associated with any
+          particular value type, and any variable can be assigned (and
+          re-assigned) values of all types:
+        </p>
+      </Content>
+    </PostContainer>
+  )
 }
---
+++ src/pages/post/styles.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components'
+
+export const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const TitlePost = styled.section`
+  width: 864px;
+  margin-top: -64px;
+  padding: 40px 32px 32px 32px;
+  border-radius: 10px;
+
+  background-color: ${(props) => props.theme['base-profile']};
+
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+
+    a {
+      font-weight: 700;
+      font-size: 12px;
+      text-transform: uppercase;
+      text-decoration: none;
+      color: ${(props) => props.theme.blue};
+    }
+
+    h2 {
+      margin-top: 20px;
+      font-weight: 700;
+      font-size: 24px;
+      color: ${(props) => props.theme['base-title']};
+    }
+  }
+`
+
+export const InfosPost = styled.section`
+  display: flex;
+  gap: 32px;
+
+  span {
+    font-weight: 400;
+    font-weight: 16px;
+    color: ${(props) => props.theme['base-span']};
+  }
+`
+
+export const Content = styled.section`
+  margin-top: 40px;
+  padding: 32px;
+  width: 800px;
+
+  p {
+    font-weight: 400;
+    font-size: 16px;
+    color: ${(props) => props.theme['base-text']};
+  }
+`
